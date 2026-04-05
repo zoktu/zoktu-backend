f172f5629a3478c9a8593dce1b6c7333314c997a
rkbansal <rohitkumarbansal23@gmail.com>
2026-04-05 10:47:07 +0530

    add

--- src/index.js
+++ src/index.js
@@ -55,6 +55,15 @@ app.use(compression());
 app.use(morgan('dev'));
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
+ 
+ // Cache-Control middleware to prevent stale data/UI issues
+ app.use((req, res, next) => {
+   res.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+   res.set('Pragma', 'no-cache');
+   res.set('Expires', '0');
+   next();
+ });
+
 
 app.get('/', (req, res) => {
   res.json({ 
